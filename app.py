c7a84e869e88b8318851a30e588af3377e194cb0
suciawalia <suci.awalia35@gmail.com>
2023-11-24 16:43:43 +0700

    home&about

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('home.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/index')
+def index2():
     return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
